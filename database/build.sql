81f4107f77327e5de48e4bfee79cf98ee58391b0
salam-dalloul <salamdalloul93@gmail.com>
2017-12-19 13:10:32 +0200

    database done

--- database/build.sql
+++ database/build.sql
@@ -4,13 +4,36 @@ DROP TABLE IF EXISTS memes CASCADE;
 CREATE TABLE memes (
 id SERIAL PRIMARY KEY,
 url VARCHAR(200) NOT NULL,
-tags VARCHAR(100) NOT NULL
+auth VARCHAR(100) NOT NULL
 );
 
 
-INSERT INTO memes (url,tags) VALUES
-('http://quotesnhumor.com/wp-content/uploads/2015/09/Top-40-Funniest-Minions-Memes-funny.jpg','minion woman');
+INSERT INTO memes (url,auth) VALUES
+('http://quotesnhumor.com/wp-content/uploads/2015/09/Top-40-Funniest-Minions-Memes-funny.jpg','mhmd');
+
+DROP TABLE IF EXISTS tags CASCADE;
+
+CREATE TABLE tags (
+id SERIAL,
+meme_id INTEGER REFERENCEs memes ,
+tag VARCHAR(100) NOT NULL,
+PRIMARY KEY (id, meme_id)
+);
+
+INSERT INTO tags (meme_id, tag) VALUES
+(1, 'minion woman');
 
 
+DROP TABLE IF EXISTS users CASCADE;
+
+CREATE TABLE users (
+id SERIAL PRIMARY KEY,
+name VARCHAR(100) NOT NULL,
+password VARCHAR(200) NOT NULL
+
+);
+
+INSERT INTO users (name, password) VALUES
+('mhmd', '123456');
 
 COMMIT;
